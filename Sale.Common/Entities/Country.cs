e15f4a5549740aa72d5bbc6347ae2307f6f16683
Ahmed jawd <amm380@yahoo.com>
2020-08-16 21:32:04 +0300

    improve Context

---
+++ Sale.Common/Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sale.Common.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50 , ErrorMessage ="the field {0} must be containe less tha {1}")]
+        [Required]
+        public string Name { get; set; }
+
+    }
+}
---
+++ Sale.web/Data/DataContext.cs
@@ -0,0 +1,26 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using Sale.Common.Entities;
+
+namespace Sale.web.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext>options):base(options)
+        {
+
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>()
+               .HasIndex(c => c.Name)
+               .IsUnique();
+        }
+
+    }
+}

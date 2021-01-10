60f69cc1a78831cdf7b2a511d5034db26dcc824d
Ahmed jawd <amm380@yahoo.com>
2021-01-10 16:49:44 +0300

    api get and put orders

--- Sale.web/Controllers/Api/OrdersController.cs
+++ Sale.web/Controllers/Api/OrdersController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Sale.Common.Responses;
 using Sale.web.Data;
 using Sale.web.Data.Entities;
@@ -26,6 +27,27 @@ namespace Sale.web.Controllers.Api
         {
            _context = context;
             _userHelper = userHelper;
+        }
+        [HttpGet]
+        public async Task<IActionResult>GetOrders()
+        {
+            string email = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value;
+            User user =await _userHelper.GetUserAsync(email);
+            if(user==null)
+            {
+                return NotFound("Error001");
+            }
+            List<Order> orders = await _context.Orders.Include(o => o.User)
+                .ThenInclude(u => u.City).Include(o => o.OrderDetails)
+                .ThenInclude(od => od.Product)
+                .ThenInclude(p => p.Category)
+                .Include(o => o.OrderDetails)
+                .ThenInclude(od => od.Product)
+                .ThenInclude(p => p.ProductImages)
+                .Where(o => o.User.Id == user.Id)
+                .OrderByDescending(o => o.Date)
+                .ToListAsync();
+            return Ok(orders);
         }
 
         [HttpPost]
@@ -69,6 +91,38 @@ namespace Sale.web.Controllers.Api
             _context.Orders.Add(order);
             await _context.SaveChangesAsync();
             return Ok(order);
+        }
+        [HttpPut]
+        public async Task<IActionResult> PutOrders([FromBody] Order order)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            string email = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value;
+            User user = await _userHelper.GetUserAsync(email);
+            if(user==null)
+            {
+                return NotFound("Error001");
+            }
+            Order currentOrder= await _context.Orders.Include(o => o.User)
+                .ThenInclude(u => u.City)
+                .Include(o => o.OrderDetails)
+                .ThenInclude(od => od.Product)
+                .ThenInclude(p => p.Category)
+                .Include(o => o.OrderDetails)
+                .ThenInclude(od => od.Product)
+                .ThenInclude(p => p.ProductImages)
+                .FirstOrDefaultAsync(o => o.Id == order.Id && o.User.Id == user.Id);
+            if(currentOrder ==null)
+            {
+                return NotFound();
+            }
+            currentOrder.OrderStatus = order.OrderStatus;
+            currentOrder.Remarks = order.Remarks;
+            _context.Orders.Update(currentOrder);
+            await _context.SaveChangesAsync();
+            return Ok(currentOrder);
         }
     }
 }
